5905dedecc5ef2e02268805a8948f2a2a86e4ab8
keitakn <keita.koga.work@gmail.com>
2018-05-24 01:50:40 +0900

    modify #3 TypeScriptに合わせてステータスコードを200に修正

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ const awsLambdaNodeLogger = require("@nekonomokochan/aws-lambda-node-logger");
 
 module.exports.jsTest = (event, context, callback) => {
   const response = {
-    statusCode: 500,
+    statusCode: 200,
     body: JSON.stringify({
       message: 'JavaScript Test',
       input: event,
